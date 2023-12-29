9df7069e9d5ed2fc897209c863b703ba41996e4f
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-12-29 22:00:33 +0100

    feat(lsp): add highlighting of symbol under cursor

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -107,3 +107,35 @@ vim.api.nvim_create_autocmd('ModeChanged', {
   desc = 'Enable diagnostics when leaving insert mode',
   callback = function(e) vim.diagnostic.enable(e.buf) end
 })
+
+-- Highlight symbol under cursor
+vim.opt.updatetime = 400
+
+local function highlight_symbol(event)
+  local id = vim.tbl_get(event, 'data', 'client_id')
+  local client = id and vim.lsp.get_client_by_id(id)
+  if client == nil or not client.supports_method('textDocument/documentHighlight') then
+    return
+  end
+
+  local group = vim.api.nvim_create_augroup('highlight_symbol', { clear = false })
+
+  vim.api.nvim_clear_autocmds({ buffer = event.buf, group = group })
+
+  vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    group = group,
+    buffer = event.buf,
+    callback = vim.lsp.buf.document_highlight,
+  })
+
+  vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+    group = group,
+    buffer = event.buf,
+    callback = vim.lsp.buf.clear_references,
+  })
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'Setup highlight symbol',
+  callback = highlight_symbol,
+})
