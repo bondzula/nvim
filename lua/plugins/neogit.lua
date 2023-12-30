f6aa3f41bf5f0357664e77d5690613961cbfd307
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-12-30 01:28:44 +0100

    feat: improve neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,11 +1,18 @@
 return {
   {
     "NeogitOrg/neogit",
-    dependencies = "nvim-lua/plenary.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
     cmd = "Neogit",
     keys = {
       { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
     },
-    config = true,
+    opts = {
+      disable_hint = true,
+      kind = "replace",
+    },
   },
 }
