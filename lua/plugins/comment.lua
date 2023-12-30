5027483c2a696539fb29d46b548b837d3889db4f
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-12-30 02:12:24 +0100

    fix: comment string changes

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,32 +3,9 @@ return {
   {
     "numToStr/Comment.nvim",
     event = "VeryLazy",
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
     opts = {
-      pre_hook = function(ctx)
-        local U = require 'Comment.utils'
-
-        -- Determine whether to use linewise or blockwise commentstring
-        local type = ctx.ctype == U.ctype.linewise and '__default' or '__multiline'
-
-        -- Determine the location where to calculate commentstring from
-        local location = nil
-        if ctx.ctype == U.ctype.blockwise then
-          location = {
-            ctx.range.srow - 1,
-            ctx.range.scol,
-          }
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require('ts_context_commentstring.utils').get_visual_start_location()
-        end
-
-        return require('ts_context_commentstring.internal').calculate_commentstring {
-          key = type,
-          location = location,
-        }
-      end,
+      -- Not sure why this is not working, as per documentation
+      -- pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
     },
   },
 }
