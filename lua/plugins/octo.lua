6dd78ee877bb1809d6f20dcffb5f24bed7d513da
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-12-30 13:28:50 +0100

    fix(octo): only define one keymap
    I'm not able to remember all of them anyway

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -1,30 +1,24 @@
--- Octo.nvim - GitHub CLI for Neovim
 return {
-  {
-    "pwntester/octo.nvim",
-    cmd = "Octo",
-    keys = {
-      { "<leader>is",  "<cmd>Octo issue list<cr>",   desc = "Issue Search" },
-      { "<leader>in",  "<cmd>Octo issue create<cr>", desc = "New Issue" },
-      { "<leader>gpl", "<cmd>Octo pr list<cr>",      desc = "List Pull Requests" },
-    },
-    opts = {
+  "pwntester/octo.nvim",
+  cmd = "Octo",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("octo").setup({
+      enable_builtin = true,
       mappings = {
-        issue = {
-          reload = { lhs = "<C-r>", desc = "Reload Issue" },
-          copy_url = { lhs = "<C-y>", desc = "Copy URL" },
-          list_issues = { lhs = "<space>is", desc = "Issue Search" },
-          close_issue = { lhs = "<space>ic", desc = "Close Issue" },
-          reopen_issue = { lhs = "<space>io", desc = "Reopen Issue" },
-          add_assignee = { lhs = "<space>iaa", desc = "Add Assignee" },
-          remove_assignee = { lhs = "<space>ida", desc = "Remove Assignee" },
-          add_label = { lhs = "<space>ial", desc = "Add Label" },
-          remove_label = { lhs = "<space>idl", desc = "Remove Label" },
-          goto_issue = { lhs = "<space>ig", desc = "navigate to a local repo issue" },
-          add_comment = { lhs = "<space>iac", desc = "Add Comment" },
-          delete_comment = { lhs = "<space>idc", desc = "Delete Comment" },
+        review_diff = {
+          select_next_entry = { lhs = "<Tab>", desc = "move to previous changed file" },
+          select_prev_entry = { lhs = "<S-Tab>", desc = "move to next changed file" },
         },
       },
-    },
+    })
+    vim.treesitter.language.register("markdown", "octo")
+  end,
+  keys = {
+    { "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
   },
 }
