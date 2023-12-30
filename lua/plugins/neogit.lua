91a92d5a9218a51c4e27a37d0af6f246297cb2e7
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-12-30 12:59:05 +0100

    feat(neogit): nicer icons, diffview, and mappings

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,18 +1,24 @@
 return {
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "nvim-telescope/telescope.nvim",
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+  },
+  cmd = "Neogit",
+  opts = {
+    signs = {
+      hunk = { "", "" },
+      item = { "", "" },
+      section = { "", "" },
     },
-    cmd = "Neogit",
-    keys = {
-      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+    integrations = {
+      diffview = true,
     },
-    opts = {
-      disable_hint = true,
-      kind = "replace",
-    },
+    disable_hint = true,
+    kind = "replace",
+  },
+  keys = {
+    { "<leader>gc", "<cmd>lua require('neogit').open({'commit'})<CR>", desc = "Git commit" },
+    { "<leader>gg", "<cmd>lua require('neogit').open()<CR>", desc = "Git commit" },
   },
 }
