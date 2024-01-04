c16d5dc818a30c31b702dd8f28e7c8f22b36511e
Stefan Bondzulic <stefanbondzulic@gmail.com>
2024-01-05 00:26:43 +0100

    feat: add git-worktree plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,27 +6,43 @@ return {
     dependencies = {
       { "nvim-lua/plenary.nvim" },
       { "natecraddock/telescope-zf-native.nvim" },
+      { "ThePrimeagen/git-worktree.nvim" },
     },
     keys = {
-      { "<C-p>",      "<cmd>Telescope find_files<cr>",                    desc = "Find Files (root dir)" },
-      { "<leader>,",  "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
-      { "<leader>/",  "<cmd>Telescope live_grep<cr>",                     desc = "Diagnostics" },
-      { "<leader>bb", "<cmd>Telescope buffers<cr>",                       desc = "Buffers" },
+      { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files (root dir)" },
+      { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
+      { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Diagnostics" },
+      { "<leader>bb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
       -- search
-      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>",     desc = "Buffer" },
-      { "<leader>sc", "<cmd>Telescope command_history<cr>",               desc = "Command History" },
-      { "<leader>sC", "<cmd>Telescope commands<cr>",                      desc = "Commands" },
-      { "<leader>sd", "<cmd>Telescope diagnostics<cr>",                   desc = "Diagnostics" },
-      { "<leader>sg", "<cmd>Telescope live_grep<cr>",                     desc = "Diagnostics" },
-      { "<leader>sh", "<cmd>Telescope help_tags<cr>",                     desc = "Help Pages" },
-      { "<leader>sH", "<cmd>Telescope highlights<cr>",                    desc = "Search Highlight Groups" },
-      { "<leader>sk", "<cmd>Telescope keymaps<cr>",                       desc = "Key Maps" },
-      { "<leader>sm", "<cmd>Telescope marks<cr>",                         desc = "Jump to Mark" },
-      { "<leader>so", "<cmd>Telescope vim_options<cr>",                   desc = "Options" },
+      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+      { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+      { "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+      { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Diagnostics" },
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+      { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+      { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+      { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
       -- LSP
-      { "<leader>cr", "<cmd>Telescope lsp_references<cr>",                desc = "Lsp References" },
-      { "<leader>cs", "<cmd>Telescope lsp_document_symbols<cr>",          desc = "Lsp Document Symbols" },
-      { "<leader>cs", "<cmd>Telescope lsp_workspace_symbols<cr>",         desc = "Lsp Workspace Symbols" },
+      { "<leader>cr", "<cmd>Telescope lsp_references<cr>", desc = "Lsp References" },
+      { "<leader>cs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Lsp Document Symbols" },
+      { "<leader>cs", "<cmd>Telescope lsp_workspace_symbols<cr>", desc = "Lsp Workspace Symbols" },
+
+      {
+        "<leader>gw",
+        function()
+          require("telescope").extensions.git_worktree.git_worktrees()
+        end,
+        desc = "Switch git worktree",
+      },
+      {
+        "<leader>gW",
+        function()
+          require("telescope").extensions.git_worktree.create_git_worktree()
+        end,
+        desc = "Create git worktree",
+      },
     },
     opts = {
       defaults = {
@@ -71,6 +87,7 @@ return {
 
       telescope.setup(opts)
       telescope.load_extension("zf-native")
+      telescope.load_extension("git_worktree")
     end,
   },
 }
