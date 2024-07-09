e45739c94198f44245510fbb7cd6f0753f63bf74
Stefan Bondzulic <stefanbondzulic@gmail.com>
2024-07-09 17:22:05 +0200

    chore: format surround file

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,10 +1,9 @@
--- TODO: document and write flash card for those keybinds
 return {
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
     config = function(_, opts)
       require("nvim-surround").setup(opts)
-    end
+    end,
   },
 }
