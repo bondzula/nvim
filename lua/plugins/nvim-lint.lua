01d01748b20f670715837ceb9bdc4edea8bb76ee
Stefan Bondzulic <stefanbondzulic@gmail.com>
2024-07-10 08:42:27 +0200

    chore(lint): remove unused function

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -5,10 +5,6 @@ return {
       linters_by_ft = {},
     },
     config = function(_, opts)
-      local function get_file_name()
-        return vim.api.nvim_buf_get_name(0)
-      end
-
       require("lint").linters_by_ft = opts.linters_by_ft
 
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
