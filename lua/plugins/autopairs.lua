cce1ce2a1118c2d58308f57d4b6323033725420f
Stefan Bondzulic <stefanbondzulic@gmail.com>
2024-07-11 15:41:26 +0200

    feat(editor): add plugin for auto closing of tags

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,43 +1,69 @@
 return {
-  "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
-  dependencies = {
-    "hrsh7th/nvim-cmp",
+  {
+    "windwp/nvim-autopairs",
+    event = { "InsertEnter" },
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      -- import nvim-autopairs
+      local autopairs = require("nvim-autopairs")
+
+      -- configure autopairs
+      autopairs.setup({
+        check_ts = true, -- enable treesitter
+        ts_config = {
+          lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+          javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+          java = false, -- don't check treesitter on java
+        },
+        fast_wrap = {
+          map = "<M-e>",
+          chars = { "{", "[", "(", '"', "'" },
+          pattern = [=[[%'%"%>%]%)%}%,]]=],
+          end_key = "$",
+          before_key = "h",
+          after_key = "l",
+          cursor_pos_before = true,
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          manual_position = true,
+          highlight = "Search",
+          highlight_grey = "Comment",
+        },
+      })
+
+      -- import nvim-autopairs completion functionality
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+      -- import nvim-cmp plugin (completions plugin)
+      local cmp = require("cmp")
+
+      -- make autopairs and completion work together
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
   },
-  config = function()
-    -- import nvim-autopairs
-    local autopairs = require("nvim-autopairs")
 
-    -- configure autopairs
-    autopairs.setup({
-      check_ts = true, -- enable treesitter
-      ts_config = {
-        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-        java = false, -- don't check treesitter on java
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      opts = {
+        -- Defaults
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = true, -- Auto close on trailing </
       },
-      fast_wrap = {
-        map = "<M-e>",
-        chars = { "{", "[", "(", '"', "'" },
-        pattern = [=[[%'%"%>%]%)%}%,]]=],
-        end_key = "$",
-        before_key = "h",
-        after_key = "l",
-        cursor_pos_before = true,
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        manual_position = true,
-        highlight = "Search",
-        highlight_grey = "Comment",
-      },
-    })
-
-    -- import nvim-autopairs completion functionality
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-    -- import nvim-cmp plugin (completions plugin)
-    local cmp = require("cmp")
-
-    -- make autopairs and completion work together
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
+      -- Also override individual filetype configs, these take priority.
+      -- Empty by default, useful if one of the "opts" global settings
+      -- doesn't work well in a specific filetype
+      -- per_filetype = {
+      --   ["html"] = {
+      --     enable_close = true,
+      --   },
+      -- },
+    },
+  },
 }
