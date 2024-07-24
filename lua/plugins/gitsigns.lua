f784e279c3c506cbb5bd4074e45a2c48f3c86907
Stefan Bondzulic <stefanbondzulic@gmail.com>
2024-07-24 09:46:56 +0200

    feat(gitsigns): improve the key-mappings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -20,17 +20,23 @@ return {
         end
 
         -- Navigation
-        map("n", "]g", gs.next_hunk, "Next Hunk")
-        map("n", "[g", gs.prev_hunk, "Prev Hunk")
+        map("n", "]h", gs.next_hunk, "Goto next [h]unk")
+        map("n", "[h", gs.prev_hunk, "Goto previous [h]unk")
 
-        map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-        map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-        map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
-        map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
-        map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
-        map("n", "<leader>gk", gs.preview_hunk, "Preview Hunk")
-        map("n", "<leader>gd", gs.diffthis, "Diff This")
-        map("n", "<leader>gb", gs.toggle_current_line_blame, "Toggle line blame")
+        -- Staging and resetting
+        map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "[G]it [S]tage hunk")
+        map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "[G]it [R]eset hunk")
+        map("n", "<leader>gS", gs.stage_buffer, "[G]it [S]tage buffer")
+        map("n", "<leader>gu", gs.undo_stage_hunk, "[G]it [U]ndo stage hunk")
+        map("n", "<leader>gR", gs.reset_buffer, "[G]it [R]eset buffer")
+
+        -- Viewing changes
+        map("n", "<leader>gk", gs.preview_hunk, "[G]it Preview hun[K]")
+        map("n", "<leader>gd", gs.diffthis, "[G]it [D]iff this")
+
+        -- Toggles
+        map("n", "<leader>tb", gs.toggle_current_line_blame, "[T]oggle Git [B]lame")
+        map("n", "<leader>td", gs.toggle_deleted, "[T]oggle [D]eleted lines")
 
         -- Text object
         map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
