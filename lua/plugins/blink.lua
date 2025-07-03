74fcaa315c8cf93db058a0580b7d6e326ff282f4
Stefan Bondzulic <stefanbondzulic@gmail.com>
2025-07-03 19:19:54 +0200

    feat(blink): add git completion to git

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,45 +1,44 @@
 return {
   {
     "saghen/blink.cmp",
-    -- optional: provides snippets for the snippet source
-    -- dependencies = "rafamadriz/friendly-snippets",
-    dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
+    dependencies = {
+      { "L3MON4D3/LuaSnip", version = "v2.*" },
+      { "Kaiser-Yang/blink-cmp-git" },
+    },
     version = "1.*",
     opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- see the "default configuration" section below for full documentation on how to define
-      -- your own keymap.
       keymap = { preset = "default" },
 
       appearance = {
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",
       },
 
       snippets = { preset = "luasnip" },
 
-      -- default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        -- default = { "lsp", "path", "snippets", "buffer" },
-        -- optionally disable cmdline completions
-        -- cmdline = {},
-        default = { "lsp", "snippets", "buffer", "path" },
+        default = { "lsp", "snippets", "path", "buffer" },
+
+        per_filetype = {
+          gitcommit = { "git", "snippets", "path", "buffer" },
+        },
+
+        providers = {
+          git = {
+            module = "blink-cmp-git",
+            name = "Git",
+            opts = {},
+          },
+        },
       },
 
-      -- experimental signature help support
       signature = { enabled = true },
     },
-    -- allows extending the providers array elsewhere in your config
-    -- without having to redefine it
     opts_extend = { "sources.default" },
   },
+
   {
     "saghen/blink.pairs",
-    version = "*", -- (recommended) only required with prebuilt binaries
+    version = "*",
     dependencies = "saghen/blink.download",
     opts = {
       mappings = {
